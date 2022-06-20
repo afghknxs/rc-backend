46122ce9a02a298975c60bcef12efd60163fabf2
tsexr7hxyf <107862621+tsexr7hxyf@users.noreply.github.com>
2022-06-21 00:20:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,12 @@ then
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
 
-sh -c "rclone $*"
+#Infinite run 
+
+while :
+do
+	rclone copy "smalltd:" "onedrive:" --transfers 5 -P --ignore-existing --drive-chunk-size 256M --onedrive-chunk-size=5M --drive-acknowledge-abuse --drive-stop-on-upload-limit
+	sleep 3
+done
+
+#sh -c "rclone $*"
