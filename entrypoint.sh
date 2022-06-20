5bbc282837f33da63c70004dd5430a0c52666b73
tsexr7hxyf <107862621+tsexr7hxyf@users.noreply.github.com>
2022-06-21 00:23:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 
 while :
 do
-	rclone copy "smalltd:" "onedrive:" --transfers 5 -P --ignore-existing --drive-chunk-size 256M --onedrive-chunk-size=5M --drive-acknowledge-abuse --drive-stop-on-upload-limit
+	rclone sync "smalltd:" "onedrive:" --transfers 5 -P --ignore-existing --drive-chunk-size 256M --onedrive-chunk-size=5M --drive-acknowledge-abuse --drive-stop-on-upload-limit
 	sleep 3
 done
 
